cfaac6b0e14a05d18ed1be611e43d2950f998ee2
Florian Beeres <yuuki@protonmail.com>
2020-07-17 00:11:58 +0200

    Add integrant
    I want to start the application with proper layering right from the
    start. After looking at component mount integrant and commix it seemed
    that integrant strikes the best balance between community support, and
    ease of use.

--- api/src/notes/api.clj
+++ api/src/notes/api.clj
@@ -3,16 +3,35 @@
   (:require [ring.middleware.defaults :refer [wrap-defaults api-defaults]])
   (:require [ring.util.response :refer [response]])
   (:require [ring.middleware.json :refer [wrap-json-response]])
-  (:require [compojure.core :refer [defroutes GET]])
+  (:require [compojure.core :refer [routes GET]])
+  (:require [integrant.core :as ig])
   (:gen-class))
 
-(defroutes handler
-  (wrap-json-response
-    (GET "/" [] (response { "a" 1 }))))
+(def config
+  {:adapter/jetty {:port (Integer/valueOf (or (System/getenv "port") "3000"))
+                   :handler (ig/ref :routes/foo)}
+   :routes/foo {:base-handler (ig/ref :handler/base)}
+   :handler/base {:name "foo"}})
 
-(def api
-  (wrap-defaults handler api-defaults))
+(defmethod ig/init-key :adapter/jetty [_ {:keys [handler] :as opts}]
+  (run-jetty handler (-> opts (dissoc :handler) (assoc :join? false))))
+
+(defmethod ig/halt-key! :adapter/jetty [_ server]
+  (.stop server))
+
+(defmethod ig/init-key :handler/base [_ {:keys [name]}]
+  (fn [] (response {"name" name})))
+
+(defmethod ig/init-key :routes/foo [_ {:keys [base-handler]}]
+  (-> (routes (GET "/" [] (base-handler)))
+      (wrap-json-response)
+      (wrap-defaults api-defaults)))
+
+(def system
+  (ig/init config))
+
+(comment (ig/halt! system))
 
 (defn -main
   [& args]
-  (run-jetty api {:port (Integer/valueOf (or (System/getenv "port") "3000"))}))
+  (system))
