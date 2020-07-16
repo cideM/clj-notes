0eaeeeee0c22cf7488acf8546b0ad02242c0dd1a
Florian Beeres <yuuki@protonmail.com>
2020-07-16 16:28:14 +0200

    Initial commit

---
+++ api/src/notes/api.clj
@@ -0,0 +1,7 @@
+(ns notes.api
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ api/test/notes/api_test.clj
@@ -0,0 +1,7 @@
+(ns notes.api-test
+  (:require [clojure.test :refer :all]
+            [notes.api :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
