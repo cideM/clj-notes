c5ecbcd9f517709a78a111a84b1cbbb0b3c6cc39
Florian Beeres <yuuki@protonmail.com>
2020-07-17 18:37:39 +0200

    Add postgres
    - Add jdbc and create an integrant entity for data source
    - Add postgres docker container and adminer
    - Create a table if not exists and just run a dummy query in default
      handler

--- api/src/notes/api.clj
+++ api/src/notes/api.clj
@@ -4,26 +4,63 @@
   (:require [ring.util.response :refer [response]])
   (:require [ring.middleware.json :refer [wrap-json-response]])
   (:require [compojure.core :refer [routes GET]])
+  (:require [next.jdbc :as jdbc])
+  (:require [next.jdbc.sql :as sql])
   (:require [integrant.core :as ig])
+  (:require [hikari-cp.core :refer [make-datasource close-datasource]])
   (:gen-class))
 
 (def config
   {:adapter/jetty {:port (Integer/valueOf (or (System/getenv "port") "3000"))
                    :host (or (System/getenv "host") "0.0.0.0")
                    :handler (ig/ref :routes/foo)}
-   :routes/foo {:base-handler (ig/ref :handler/base)}
-   :handler/base {:name "foo"}})
+   :routes/foo    {:base-handler (ig/ref :handler/base)}
+   :handler/base  {:name "foo"
+                   :db (ig/ref :db/pool)}
+   :db/pool       {:adapter "postgresql"
+                   :password "password"
+                   :username "root"
+                   :server-name "db"
+                   :port-number "5432"
+                   :database-name "notes"}})
 
-(defmethod ig/init-key :adapter/jetty [_ {:keys [handler] :as opts}]
-  (run-jetty handler (-> opts (dissoc :handler) (assoc :join? false))))
+(defmethod ig/init-key
+  :db/pool
+  [_ opts]
+  (let [ds (make-datasource opts)]
+    (do (jdbc/execute! ds ["
+         create table if not exists notes (
+           id SERIAL,
+           title varchar(100),
+           body varchar(500)
+         )"])
+        ds)))
 
-(defmethod ig/halt-key! :adapter/jetty [_ server]
+(defmethod ig/halt-key! :db/pool [_ opts] (close-datasource opts))
+
+(defmethod ig/init-key
+  :adapter/jetty
+  [_ {:keys [handler] :as opts}]
+  (run-jetty handler
+    (-> opts
+        (dissoc :handler)
+        (assoc :join? false))))
+
+(defmethod ig/halt-key!
+  :adapter/jetty
+  [_ server]
   (.stop server))
 
-(defmethod ig/init-key :handler/base [_ {:keys [name]}]
-  (fn [] (response {"name" name})))
+(defmethod ig/init-key
+  :handler/base
+  [_ {:keys [name db]}]
+  (fn
+    []
+    (response (sql/query db ["select * from notes"]))))
 
-(defmethod ig/init-key :routes/foo [_ {:keys [base-handler]}]
+(defmethod ig/init-key
+  :routes/foo
+  [_ {:keys [base-handler]}]
   (-> (routes (GET "/" [] (base-handler)))
       (wrap-json-response)
       (wrap-defaults api-defaults)))
